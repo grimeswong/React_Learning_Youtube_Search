4d524b7a70ce95d0ae7080a19e54777f4710daec
Shadow Grimes <grimeswong@gmail.com>
2017-12-03 04:03:48 +1000

    Progress 1 - created a search bar component that render on the home page and created structured components js files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+//
+// import App from './components/app';
+// import reducers from './reducers';
+//
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+//
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <App />
+//   </Provider>
+//   , document.querySelector('.container'));
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+/*** Grimes ***/
+
+const API_KEY = 'AIzaSyDxEceMxjg_EwI0Vhbk1FLEve_Rt97Ku9Y';
+
+import React from 'react';          // Old React Core
+import ReactDOM from 'react-dom';   // New React that control DOM
+
+import SearchBar from './components/search_bar'; // import the searchBat component that I made. note: path for the reference
+
+
+// Step1: Create a new Component. This component should produce some HTML
+// const App = function() {     // ES5 Syntax
+const App = () => {             // ES6 Syntax
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// Step2: Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container')); // Make the instance of App (Not a class),
+// document.querySelector(".example"); = Get the first element in the document with class="example"
