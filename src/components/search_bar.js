f1892d5a06471063fbaa623cd689fdbe45ea7d09
Shadow Grimes <grimeswong@gmail.com>
2017-12-03 04:30:15 +1000

    Progress 1 - class component & alternative syntax for event handling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,19 @@
-import React from 'react';
+// import React from 'react';   Alternative syntax with below
+import React, { Component } from 'react';
 
+// class SearchBar extends React.Component { // Create a class component, inherit the functionalities from the React.Component class
+class SearchBar extends Component { // Create a class component, inherit the functionalities from the React.Component class
+  render() {    // class must declare with render() function
+    return <input onChange={this.onInputChange} />; // Must use curly braces
+                // evnet name = { event handler }
 
-const SearchBar = () => {
-  return <input />;    // React.createElement
-};
+    // return <input onChange = {event => console.log(event.target.value)} />  // simplified syntax
+  }
+
+  onInputChange(event) { //  Best practice: (handle, name of element, name of event), parameter(event object)
+    // console.log(event.target.value);   //test the input field
+    console.log(event);
+  }
+}
 
 export default SearchBar;
