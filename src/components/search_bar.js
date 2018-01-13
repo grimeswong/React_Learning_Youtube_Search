5e1895ad4c478681373537f751e22eab880c7a60
Shadow Grimes <grimeswong@gmail.com>
2018-01-13 15:06:36 +1000

    Section1: Progress in handling event @13-Jan-18

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,29 @@
 // import React from 'react';   Alternative syntax with below
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // class SearchBar extends React.Component { // Create a class component, inherit the functionalities from the React.Component class
-class SearchBar extends Component { // Create a class component, inherit the functionalities from the React.Component class
-  render() {    // class must declare with render() function
-    return <input onChange={this.onInputChange} />; // Must use curly braces
-                // evnet name = { event handler }
+class SearchBar extends Component {
+  // Create a class component, inherit the functionalities from the React.Component class
+  //initialise class state (remember functional components don't have state)
+  constructor(props) {
+    super(props);
+    this.state = { term: "" }; //innitialise state
+  }
+  render() {
+    // class must declare with render() function
+    return (
+      <input onChange={event => this.setState({ term: event.target.value })} />
+    ); // Must use curly braces
+    // evnet name = { event handler }
 
     // return <input onChange = {event => console.log(event.target.value)} />  // simplified syntax
   }
 
-  onInputChange(event) { //  Best practice: (handle, name of element, name of event), parameter(event object)
+  onInputChange(event) {
+    //  Best practice: (handle, name of element, name of event), parameter(event object)
     // console.log(event.target.value);   //test the input field
     console.log(event);
   }
 }
 
-export default SearchBar;
+export default SearchBar; // export this component
